48571b67054c5cbe2e5d4f2c61632abbf9b67a8b
lancefan <lancefan@outlook.com>
2021-02-24 18:57:30 -0800

    live on DO

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-var http = require('http')
+var http = require("http");
 var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Have fun with Node.js!\n');
-}).listen(port);
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Have fun with Node.js on Digital Ocean!\n");
+  })
+  .listen(port);
